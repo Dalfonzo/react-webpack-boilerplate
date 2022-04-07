bc4e9423e7079d906ce6fb085c6353b09184d1f2
David Pérez <dalfonzo18@gmail.com>
2022-04-07 12:14:44 -0400

    Cleanup

--- src/components/heading.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export default function Heading() {
-    return <p>Heading 2</p>
-}
--- src/index.js
+++ src/index.js
@@ -1,19 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Heading from './components/heading'
-import Paragraph from './components/paragraph'
 import './index.css'
-import './index.scss'
-import logo from './img.png'
 
 function App() {
-    return (
-        <>
-            <Heading></Heading>
-            <img src={logo}></img>
-            <Paragraph></Paragraph>
-        </>
-    )
+    return <h1>Javascript Boilerplate using webpack</h1>
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'))
